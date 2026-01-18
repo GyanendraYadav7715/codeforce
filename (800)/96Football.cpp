4a4db6ed245e57f99125cb71091ab202b8a923ce
Gyan <gyanendrayadav12345789@gmail.com>
2026-01-18 10:19:45 +0530

    96A

--- (800)/96Football.cpp
+++ (800)/96Football.cpp
@@ -1,22 +1,52 @@
-#include<iostream>
+#include <iostream>
 #include <string>
 using namespace std;
 
-int main(){
+int main()
+{
 
     string teamPlayerPostion;
     cin >> teamPlayerPostion;
     int teamA = 0;
     int teamB = 0;
-
-    for (char s : teamPlayerPostion){
-        if (s == '1' && teamA<=7){
+    // 001001
+    for (char s : teamPlayerPostion)
+    {
+        if (s == '1' )
+        {
             teamA++;
-        }else if(s == '1' && teamA <= 7)
+        }
+        else
+        { 
+            if(teamA>=7){
+                
+            }else{
+                teamA = 0;
+            }
+           
+        }
+        if (s == '0')
         {
             teamB++;
         }
+        else
+        {
+            if (teamB >=7){
+
+            }
+            else{
+                teamB = 0;
+            }
+        }
+    }
+    if (teamA >= 7 || teamB >= 7)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
     }
 
-        return 0;
+    return 0;
 }
