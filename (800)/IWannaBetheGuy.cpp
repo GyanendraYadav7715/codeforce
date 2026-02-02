f2be99b16920c6719fa8b6c1b683c1dcef517a61
Gyan <gyanendrayadav12345789@gmail.com>
2026-02-02 17:56:18 +0530

    I wann be a guy.

---
+++ (800)/IWannaBetheGuy.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    int n, p, q, level;
+    cin >> n;
+
+    set<int> unique_levels;
+
+    cin >> p;
+    for (int i = 0; i < p; i++)
+    {
+        cin >> level;
+        unique_levels.insert(level);
+    }
+
+    cin >> q;
+    for (int i = 0; i < q; i++)
+    {
+        cin >> level;
+        unique_levels.insert(level);
+    }
+
+    if (unique_levels.size() == n)
+    {
+        cout << "I become the guy." << endl;
+    }
+    else
+    {
+        cout << "Oh, my keyboard!" << endl;
+    }
+
+    return 0;
+}
