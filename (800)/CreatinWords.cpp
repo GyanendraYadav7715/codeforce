2c75ff3f314f799857a0c69fae4372617a6bf25c
Gyan <gyanendrayadav12345789@gmail.com>
2026-03-12 16:15:54 +0530

    creating word

---
+++ (800)/CreatinWords.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve()
+{
+    string a, b;
+    cin >> a >> b;
+
+    char temp = a[0];
+    a[0] = b[0];
+    b[0] = temp;
+
+    cout << a << " " << b << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
