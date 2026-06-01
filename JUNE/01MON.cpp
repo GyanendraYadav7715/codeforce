ab57541c143027d1a19eeaccf9e22c557b2f764e
Gyan <gyanendrayadav12345789@gmail.com>
2026-06-01 09:54:39 +0530

    JUNE IS ENTER IN CODEFORCE

---
+++ JUNE/01MON.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void solve()
+{
+    int coordinate;
+    cin >> coordinate;
+
+    
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
