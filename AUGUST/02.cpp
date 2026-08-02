cf079606f8c51faf9d4bbdd314c81435178c96ff
Gyan <gyanendrayadav12345789@gmail.com>
2026-08-02 22:59:29 +0530

    02/08

--- AUGUST/02.cpp
+++ AUGUST/02.cpp
@@ -1,113 +1,55 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-void solve()
-{
-    int n, k;
-    cin >> n >> k;
-
-    vector<int> a(n);
-    vector<int> sorted_a(n);
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> a[i];
-        sorted_a[i] = a[i];
-    }
-
-    // Sort to find the k-th element (1-based index k -> index k-1)
-    sort(sorted_a.begin(), sorted_a.end());
-    int v = sorted_a[k - 1];
-
-    int cnt_less = 0;
-    int cnt_equal = 0;
-    for (int x : a)
-    {
-        if (x < v)
-            cnt_less++;
-        else if (x == v)
-            cnt_equal++;
-    }
-
-    // Maximum number of elements equal to 'v' we are permitted to delete
-    int allowed_v_deletions = (cnt_less + cnt_equal) - k;
-
-    int left = 0, right = n - 1;
-    bool possible = true;
-
-    while (left < right)
-    {
-        if (a[left] == a[right])
-        {
-            left++;
-            right--;
-        }
-        else
-        {
-            // Attempt 1: Try deleting a[left]
-            bool delete_left = false;
-            if (a[left] > v)
-            {
-                delete_left = true;
-            }
-            else if (a[left] == v && allowed_v_deletions > 0)
-            {
-                delete_left = true;
-                allowed_v_deletions--;
-            }
-
-            if (delete_left)
-            {
-                left++;
-                continue;
-            }
-
-            // Attempt 2: Try deleting a[right]
-            bool delete_right = false;
-            if (a[right] > v)
-            {
-                delete_right = true;
-            }
-            else if (a[right] == v && allowed_v_deletions > 0)
-            {
-                delete_right = true;
-                allowed_v_deletions--;
-            }
-
-            if (delete_right)
-            {
-                right--;
-                continue;
-            }
-
-            // If neither end can be deleted, palindrome cannot be formed
-            possible = false;
-            break;
-        }
-    }
-
-    if (possible)
-    {
-        cout << "YES\n";
-    }
-    else
-    {
-        cout << "NO\n";
-    }
-}
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    int t;
-    cin >> t;
-    while (t--)
+    int T;
+    cin >> T;
+
+    while (T--)
     {
-        solve();
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> a(n), b;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            b.push_back(a[i]);
+        }
+
+        sort(b.begin(), b.end());
+        int lim = b[k - 1]; // k-th smallest value
+
+        int l = 0, r = n - 1;
+        bool ok = true;
+
+        while (l < r)
+        {
+            if (a[l] == a[r])
+            {
+                l++;
+                r--;
+            }
+            else if (a[l] >= lim)
+            {
+                l++; // delete left
+            }
+            else if (a[r] >= lim)
+            {
+                r--; // delete right
+            }
+            else
+            {
+                ok = false;
+                break;
+            }
+        }
+
+        cout << (ok ? "YES" : "NO") << '\n';
     }
 
     return 0;
